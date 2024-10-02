09b70c80460f53f19ee159057e310bce09b0f1d7
LiongsonEnzo <156958405+LiongsonEnzo@users.noreply.github.com>
2024-10-01 22:17:24 -0400

    Create TestStudent.java

---
+++ TestStudent.java
@@ -0,0 +1,6 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student("Bob", 20, "Male", "S12345", 15, 3.8);
+        System.out.println(student);
+    }
+}
